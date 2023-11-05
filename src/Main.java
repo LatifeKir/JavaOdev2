3500f0a616be0b376a9acf478fddb132fabe7548
Latife <latife.lkir@gmail.com>
2023-11-05 15:25:54 +0300

    Sayı Fonksiyonu Eklendi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.io.Console;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner myInput = new Scanner( System.in );
+        int a;
+        int b;
+
+        System.out.println("Lütfen 1. Sayıyı Giriniz");
+        a= myInput.nextInt();
+
+        System.out.println("Lütfen 2. Sayıyı Giriniz");
+        b= myInput.nextInt();
+
+
+        int toplam = 0;
+
+        toplam=a+b;
+
+        if (toplam % 2 ==0)
+        {
+            System.out.println("SAYI ÇİFTTİR");
+        }
+        else {
+            System.out.println("SAYI TEKTİR");
+        }
+
     }
 }
